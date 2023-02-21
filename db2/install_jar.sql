68a599aa5fe5f29cda2fc668e850e3f937b6001f
Jeremy Rickard <30393514+jrickard27@users.noreply.github.com>
2023-02-22 01:33:58 +1300

    Create install_jar.sql

---
+++ db2/install_jar.sql
@@ -0,0 +1,9 @@
+-- Install JAR.
+CALL sqlj.install_jar('file:/home/eds/Projects/open/db2-compress/java/dist/db2-compress.jar', 'UTILS.GZIPJAR');
+
+-- Update JAR and tell Db2 to refresh classes.
+CALL sqlj.replace_jar('file:/home/eds/Projects/open/db2-compress/java/dist/db2-compress.jar', 'UTILS.GZIPJAR');
+CALL sqlj.refresh_classes();
+
+-- Remove JAR (you must drop functions first).
+CALL sqlj.remove_jar('UTILS.GZIPJAR');
