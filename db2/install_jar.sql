b9685689ef07fc3f97dd5a0efb953d2d6acc8b15
Jeremy Rickard <jeremy.rickard@easydataservices.com>
2023-04-01 15:11:06 +1300

    Change path

--- db2/install_jar.sql
+++ db2/install_jar.sql
@@ -1,8 +1,8 @@
 -- Install JAR.
-CALL sqlj.install_jar('file:/home/eds/Projects/open/db2-compress/java/dist/db2-compress.jar', 'UTILS.GZIPJAR');
+CALL sqlj.install_jar('file:/home/eds/Projects/git/db2-compress/java/dist/db2-compress.jar', 'UTILS.GZIPJAR');
 
 -- Update JAR and tell Db2 to refresh classes.
-CALL sqlj.replace_jar('file:/home/eds/Projects/open/db2-compress/java/dist/db2-compress.jar', 'UTILS.GZIPJAR');
+CALL sqlj.replace_jar('file:/home/eds/Projects/git/db2-compress/java/dist/db2-compress.jar', 'UTILS.GZIPJAR');
 CALL sqlj.refresh_classes();
 
 -- Remove JAR (you must drop functions first).
