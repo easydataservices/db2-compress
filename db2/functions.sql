116fb2e03ab2b42fba5566acd84a5105dfe8bcea
Jeremy Rickard <jeremy.rickard@easydataservices.com>
2023-04-05 01:25:21 +1200

    Add CRC32 checksum functions

--- db2/functions.sql
+++ db2/functions.sql
@@ -1,15 +1,33 @@
-CREATE OR REPLACE FUNCTION utils.gzip_compress(p_blob BLOB(2M)) RETURNS BLOB(2M)
+CREATE OR REPLACE FUNCTION utils.gzip_compress(p_blob BLOB(20M)) RETURNS BLOB(20M)
   LANGUAGE JAVA
   PARAMETER STYLE JAVA 
   FENCED
+  RETURNS NULL ON NULL INPUT
   EXTERNAL NAME 'UTILS.GZIPJAR:com.easydataservices.open.db2compress.Gzip.compressBlob';
 
-CREATE OR REPLACE FUNCTION utils.gzip_uncompress(p_compressed_blob BLOB(2M)) RETURNS BLOB(2M)
+CREATE OR REPLACE FUNCTION utils.gzip_uncompress(p_compressed_blob BLOB(20M)) RETURNS BLOB(20M)
   LANGUAGE JAVA
   PARAMETER STYLE JAVA 
   FENCED
+  RETURNS NULL ON NULL INPUT
   EXTERNAL NAME 'UTILS.GZIPJAR:com.easydataservices.open.db2compress.Gzip.uncompressBlob';
- 
+
+CREATE OR REPLACE FUNCTION utils.blob_crc32(p_blob BLOB(20M)) RETURNS BIGINT
+  LANGUAGE JAVA
+  PARAMETER STYLE JAVA 
+  FENCED
+  RETURNS NULL ON NULL INPUT
+  EXTERNAL NAME 'UTILS.GZIPJAR:com.easydataservices.open.db2compress.Gzip.blobCRC32';
+
+CREATE OR REPLACE FUNCTION utils.gzip_compressed_blob_crc32(p_compressed_blob BLOB(20M)) RETURNS BIGINT
+  LANGUAGE JAVA
+  PARAMETER STYLE JAVA 
+  FENCED
+  RETURNS NULL ON NULL INPUT
+  EXTERNAL NAME 'UTILS.GZIPJAR:com.easydataservices.open.db2compress.Gzip.compressedBlobCRC32';
+
  -- DROP FUNCTION utils.gzip_compress;
  -- DROP FUNCTION utils.gzip_uncompress;
+ -- DROP FUNCTION utils.blob_crc32;
+ -- DROP FUNCTION utils.gzip_compressed_blob_crc32;
  
--- java/src/open/db2compress/Gzip.java
+++ java/src/open/db2compress/Gzip.java
@@ -1,10 +1,12 @@
 package com.easydataservices.open.db2compress;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.zip.GZIPOutputStream;
+import java.util.zip.CRC32;
 import java.util.zip.GZIPInputStream;
 import sqlj.runtime.ref.DefaultContext;
 
@@ -37,7 +39,7 @@ public class Gzip {
       compressedBlob = context.getConnection().createBlob();  
     }
     catch (Exception exception) {
-      throw new SQLException("Failed to create Blob from default context connection: " + exception.getMessage());
+      throw new SQLException("Failed to create Blob from default context connection: " + exception.getMessage(), "72002");
     }
     
     // Compress.
@@ -56,7 +58,7 @@ public class Gzip {
       gzipOutputStream.finish();
     }
     catch (IOException exception) {
-      throw new SQLException(exception.getMessage());
+      throw new SQLException(exception.getMessage(), "72003");
     }
     return compressedBlob;
   }
@@ -80,7 +82,7 @@ public class Gzip {
       blob = context.getConnection().createBlob();  
     }
     catch (Exception exception) {
-      throw new SQLException("Failed to create Blob from default context connection: " + exception.getMessage());
+      throw new SQLException("Failed to create Blob from default context connection: " + exception.getMessage(), "72002");
     }
 
     // Uncompress.
@@ -97,8 +99,66 @@ public class Gzip {
       }
     }
     catch (IOException exception) {
-      throw new SQLException(exception.getMessage());
+      throw new SQLException(exception.getMessage(), "72003");
     }
     return blob;
+  }
+
+  /**
+   * Return CRC32 of input BLOB. In expected use the BLOB would be uncompressed.
+   * @param blob BLOB.
+   * @return CRC32 checksum.
+   */
+  public static long blobCRC32(Blob blob) throws SQLException {
+    // Throw exception for null input.
+    if (blob == null) {
+      throw new SQLException("Input cannot be null", "72001");
+    }
+  
+    // Return CRC32 value.
+    CRC32 crc32 = new CRC32();
+    byte[] bytes = new byte[BUFSIZE];
+    int index = 0;
+    while (index < blob.length()) {
+      int length = BUFSIZE;
+      if (blob.length() - index < BUFSIZE) {
+        length = (int) blob.length() - index;
+      }
+      bytes = blob.getBytes(index + 1, length);
+      crc32.update(bytes, 0, length);
+      index = index + length;
+    }
+    return crc32.getValue();
+  }
+
+  /**
+   * Uncompress input BLOB and return CRC32 of result.
+   * @param compressedBlob Compressed BLOB.
+   * @return CRC32 checksum of uncompressed BLOB.
+   */
+  public static long compressedBlobCRC32(Blob compressedBlob) throws SQLException {
+    // Throw exception for null input.
+    if (compressedBlob == null) {
+      throw new SQLException("Input cannot be null", "72001");
+    }
+
+    // Return CRC32 value.
+    CRC32 crc32 = new CRC32();
+    byte[] bytes = new byte[BUFSIZE];
+    try (GZIPInputStream gzipInputStream = new GZIPInputStream(compressedBlob.getBinaryStream(), GZIPBUFSIZE)) {
+      int index = 0;
+      int length = 1;
+      while (length > 0) {
+        length = gzipInputStream.read(bytes, 0, BUFSIZE);
+        if (length > 0) {
+          crc32.update(bytes, 0, length);
+          index = index + length;
+        }
+      }
+    }
+    catch (IOException exception) {
+      throw new SQLException(exception.getMessage(), "72003");
+    }
+    return crc32.getValue();
   }
 }
